2d63abff8d62f2fb96be2f5bc5ff8533c33600f9
D-714 <124953914+Ricardogs2002@users.noreply.github.com>
2023-05-13 17:34:18 -0600

    0001_Ricardo_Ismael_Gómez_Sánchez
    Subida del capítulo

---
+++ 0002_gramáticas_probab._independ._del_contexto.py
@@ -0,0 +1,26 @@
+
+import nltk
+
+# Definir una gramática CFG
+gramatica = nltk.CFG.fromstring("""
+    S -> NP VP
+    NP -> Det N
+    VP -> V NP
+    Det -> 'el' | 'un'
+    N -> 'perro' | 'gato'
+    V -> 'persigue' | 'salta'
+""")
+
+# Crear un analizador de la gramática CFG
+analizador = nltk.ChartParser(gramatica)
+
+# Frase de entrada para analizar
+frase = "el perro persigue un gato"
+
+# Tokenizar la frase en una lista de palabras
+palabras = nltk.word_tokenize(frase)
+
+# Analizar la frase utilizando el analizador de la gramática
+for arbol in analizador.parse(palabras):
+    print(arbol)
+    arbol.pretty_print()
